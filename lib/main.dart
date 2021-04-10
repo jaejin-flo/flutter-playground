9aadcbba446f8e5f98a0f57df1eb4a608fcd012f
jaejin-flo <jaejin@dreamus.io>
2021-04-10 15:09:20 +0900

    Add request api logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,29 @@
 // Copyright 2020, the Flutter project authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
+import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:http/http.dart' as http;
 
 import 'src/app.dart';
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  Album({@required this.userId, @required this.id, @required this.title});
+
+  factory Album.fromJson(dynamic json) {
+    return Album(
+      userId: json['userId'] as int,
+      id: json['id'] as int,
+      title: json['title'] as String,
+    );
+  }
+}
 
 void main() {
   // runApp(DashboardApp.mock());
@@ -29,35 +47,70 @@ class MyApp extends StatelessWidget {
 }
 // #enddocregion MyApp
 
+Future getProjectDetails() async {
+  List<Album> myModels;
+
+  var response =
+      await http.get(Uri.https('jsonplaceholder.typicode.com', 'albums'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+
+    myModels=(json.decode(response.body) as List).map((dynamic i) => Album.fromJson(i)).toList();
+    // return Album.fromJson(jsonDecode(response.body));
+
+    return myModels;
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
 // #docregion RWS-var
 class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = <WordPair>[];
+  final _saved = <String>[];
   final _biggerFont = TextStyle(fontSize: 18.0);
-  // #enddocregion RWS-var
 
   // #docregion _buildSuggestions
   Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
 
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
+    return FutureBuilder<dynamic>(
+      future: getProjectDetails(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+            padding: EdgeInsets.all(16.0),
+            itemCount: 100,
+            itemBuilder: (context, i) {
+              dynamic project = snapshot.data[i];
+              // return Column(
+              //   children: <Widget>[
+              //     ListTile(
+              //       leading: Icon(Icons.map),
+              //       title: Text(project.title as String),
+              //     ),
+              //   ],
+              // );
+              return _buildRow(project.title as String);
+            },
+          );
+        }
+        return Container();
+      },
+    );
+
   }
-  // #enddocregion _buildSuggestions
+
 
   // #docregion _buildRow
-  Widget _buildRow(WordPair pair) {
+  Widget _buildRow(String pair) {
     final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
-        pair.asPascalCase,
+        // pair.asPascalCase,
+        pair,
         style: _biggerFont,
       ),
       trailing: Icon(
@@ -75,6 +128,7 @@ class _RandomWordsState extends State<RandomWords> {
       },
     );
   }
+
   // #enddocregion _buildRow
 
   // #docregion RWS-build
@@ -92,33 +146,31 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final tiles = _saved.map(
-                (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      final tiles = _saved.map(
+        (String pair) {
+          return ListTile(
+            title: Text(
+              // pair.asPascalCase,
+              pair,
+              style: _biggerFont,
             ),
-            body: ListView(children: divided),
           );
-        }
-      )
-    );
+        },
+      );
+      final divided = ListTile.divideTiles(
+        context: context,
+        tiles: tiles,
+      ).toList();
+
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Saved Suggestions'),
+        ),
+        body: ListView(children: divided),
+      );
+    }));
   }
 // #enddocregion RWS-build
 // #docregion RWS-var
